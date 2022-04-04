ad9e1b780de3fc1366384aec0cd5b7213e62e13b
shinohara <>
2022-04-04 11:08:46 +0900

    ・migration file, DB の作成 ・Task.7-2 ルーティングの実装 ・Task.7-3 article作成→表示(index, show, new, create) ・Task.7-4 article編集(edit, update)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: %i[ show edit update ]
 
   # GET /articles or /articles.json
   def index
@@ -17,34 +17,31 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+    @article = Article.find(params[:id])
   end
 
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
+    # respond_to do |format|
+      if @article.save!
+        redirect_to article_url(@article), notice: "Article was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
-    end
+    # end
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
+    # respond_to do |format|
+      if @article.update!(article_params)
+        redirect_to article_url(@article), notice: "Article was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
-    end
+    # end
   end
 
   # DELETE /articles/1 or /articles/1.json
@@ -52,8 +49,7 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
+      redirect_to articles_url, notice: "Article was successfully destroyed."
     end
   end
 
