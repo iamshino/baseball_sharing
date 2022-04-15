8f6c9f27653a5fb9b03257ac20e0d29d36fb4f08
shinohara <>
2022-04-15 12:24:29 +0900

    Task.10-2

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  def detail
-    @user = User.find_by(id: params[:id])
-  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -54,9 +51,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    "/user/#{current_user.id}"
-  end
+  # def after_sign_up_path_for(resource)
+  #   "/user/#{current_user.id}"
+  # end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
