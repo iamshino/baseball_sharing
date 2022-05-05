d12ef6bdd40f14f89f01a1d7e85f7b7b139b3fdd
shinohara <>
2022-05-05 17:51:06 +0900

    Task.14 検索フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get '/mypage', to: 'mypage#show'
   devise_for :users
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    collection do
+      get 'search'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
