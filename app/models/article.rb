309a84aa9b7b038250cf07b2819698e0e7673501
shinohara <>
2022-05-07 10:19:16 +0900

    Task.15-2 記事とタグのアソシエーションの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
+  has_many :tags, through: :tag_articles
+  has_many :tag_articles, dependent: :destroy
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
+  has_many :articles, through: :tag_articles
+  has_many :tag_articles, dependent: :destroy
 end
