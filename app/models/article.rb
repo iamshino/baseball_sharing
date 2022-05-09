7e4313d7afd3a62561c2f57f778ba10e3587a8e0
shinohara <>
2022-05-10 08:19:52 +0900

    Task.15-3 テンプレート

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
-  has_many :tags, through: :tag_articles
   has_many :tag_articles, dependent: :destroy
+  has_many :tags, through: :tag_articles
 end
