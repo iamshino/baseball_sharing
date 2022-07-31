6d3b9ce77dfc1d34140757ed9fb4c1a1615f1031
shinohara <>
2022-07-31 15:31:10 +0900

    modelのテストの準備

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Tag, type: :model do
+  context "nameを指定している時" do
+    it "タグが作成できる" do
+    end
+  end
+
+  context "nameを指定していない時" do
+    it "エラーする" do
+    end
+  end
+end
